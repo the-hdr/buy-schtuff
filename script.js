4a1fe29049bb7c2182691d6c08d44740b8a6870d
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-08-01 20:20:26 +0530

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, push } from "https://www.gstatic.com/firebaseejs/9.15.0/firebase-database.js"
+
 const appSettings = {
     databaseURL: "https://buy-schtuff-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
-function logText()
-{
-    const textContent = document.getElementById("text-input").value;
-    console.log (textContent);
-}
+const app = initializeApp (appSettings)
+const database = getDatabase (app)
+const moviesInDb = ref (database, "movies")
+
+const inputFieldElement = document.getElementById ("input-field")
+const addButtonElement = document.getElementById ("add-button")
+
+addButtonElement.addEventListener ("click", function() {
+    let inputValue = inputFieldElement.value
+
+    push (moviesInDb, inputValue)
+
+    console.log (`${inputValue} added to database`)
+})
