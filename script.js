7120d93a063bf4f4f300281b5955d327064f56b9
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2024-08-01 19:36:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const appSettings = {
+    databaseURL: "https://buy-schtuff-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+function logText()
+{
+    const textContent = document.getElementById("text-input").value;
+    console.log (textContent);
+}
